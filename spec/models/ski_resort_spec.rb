95b90e6e47fc43b2ffeea2996163b7eaa42e37b5
Sam Iavri <sammi80134@gmail.com>
2022-02-03 17:03:24 -0700

    added validation to model testing

--- spec/models/ski_resort_spec.rb
+++ spec/models/ski_resort_spec.rb
@@ -4,4 +4,15 @@ RSpec.describe SkiResort, type: :model do
   describe 'relationships' do
     it { should have_many(:ski_runs) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :lifts }
+    it { should validate_presence_of :backcountry_access }
+    it { should validate_presence_of :employee }
+    it { should validate_presence_of :snowboarder_permitted }
+    it { should validate_presence_of :altitude }
+    it { should validate_presence_of :avg_snowfall }
+    it { should validate_presence_of :location }
+  end
 end
