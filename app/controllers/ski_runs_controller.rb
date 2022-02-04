1f696cd172e880838f87c2f95db4030fc3aa4bde
Sam Iavri <sammi80134@gmail.com>
2022-02-03 18:06:54 -0700

    User story 4 completed with passing test

--- app/controllers/ski_runs_controller.rb
+++ app/controllers/ski_runs_controller.rb
@@ -2,4 +2,8 @@ class SkiRunsController < ApplicationController
   def index
     @ski_runs = SkiRun.all
   end
+
+  def show
+    @ski_run = SkiRun.find_by_id(params[:id])
+  end
 end
--- spec/features/ski_runs/show_spec.rb
+++ spec/features/ski_runs/show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe 'ski run index' do
-  it 'lists ski runs name and other attributes' do
+RSpec.describe 'ski run show' do
+  it 'shows the ski run name and other attributes' do
     keystone = SkiResort.create!(name: 'Keystone', lifts: 20, backcountry_access: true, employee: 30_000,
                                  snowboarder_permitted: true, altitude: 9280, avg_snowfall: 235, location: 'Summit County')
     schoolmarm = keystone.ski_runs.create!(name: 'schoolmarm', open: true, distance: 3, green: true, blue: false,
@@ -9,14 +9,15 @@ RSpec.describe 'ski run index' do
     noodle_soup = keystone.ski_runs.create!(name: 'noodle soup', open: false, distance: 2, green: false, blue: false,
                                             black: true, condition: 'groomed')
 
-    visit '/ski_runs'
+    visit "/ski_runs/#{noodle_soup.id}"
 
-    expect(page).to have_content(schoolmarm.name.titleize)
-    expect(page).to have_content(schoolmarm.open)
-    expect(page).to have_content(schoolmarm.distance)
-    expect(page).to have_content(schoolmarm.green)
-    expect(page).to have_content(schoolmarm.blue)
-    expect(page).to have_content(schoolmarm.black)
-    expect(page).to have_content(schoolmarm.condition)
+    expect(page).to have_content(noodle_soup.name.titleize)
+    expect(page).to have_content(noodle_soup.open)
+    expect(page).to have_content(noodle_soup.distance)
+    expect(page).to have_content(noodle_soup.green)
+    expect(page).to have_content(noodle_soup.blue)
+    expect(page).to have_content(noodle_soup.black)
+    expect(page).to have_content(noodle_soup.condition)
+    expect(page).to_not have_content(schoolmarm.name)
   end
 end
