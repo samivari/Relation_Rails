6af98284d82bf0ad70e3937feb451972062e7a16
Sam Iavri <sammi80134@gmail.com>
2022-02-08 10:27:41 -0700

    User story 13

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   get '/ski_resorts/:id', to: 'ski_resorts#show'
   get '/ski_runs', to: 'ski_runs#index'
   get '/ski_runs/:id', to: 'ski_runs#show'
-  get 'ski_resorts/:ski_resort_id/ski_runs', to: 'ski_resort_runs#index'
-  get 'ski_resorts/:id/edit', to: 'ski_resorts#edit'
+  get '/ski_resorts/:ski_resort_id/ski_runs/new', to: 'ski_resort_runs#new'
+  post 'ski_resorts/:ski_resort_id/ski_runs', to: 'ski_resort_runs#create'
+  get '/ski_resorts/:ski_resort_id/ski_runs', to: 'ski_resort_runs#index'
+  get '/ski_resorts/:id/edit', to: 'ski_resorts#edit'
   post '/ski_resorts', to: 'ski_resorts#create'
   patch '/ski_resorts/:id', to: 'ski_resorts#update'
 end
