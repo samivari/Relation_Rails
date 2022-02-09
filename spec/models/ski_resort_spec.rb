a64dd48558a9af528ed979dfafc78a759cdd6ff6
Sam Iavri <sammi80134@gmail.com>
2022-02-09 13:25:22 -0700

    model testing complete

--- spec/models/ski_resort_spec.rb
+++ spec/models/ski_resort_spec.rb
@@ -29,4 +29,18 @@ RSpec.describe SkiResort, type: :model do
       end
     end
   end
+
+  describe 'instance methods' do
+    describe '#count runs' do
+      it 'counts the run at a resort' do
+        keystone = SkiResort.create!(name: 'Keystone', lifts: 20, backcountry_access: true, employee: 30_000,
+                                     snowboarder_permitted: true, altitude: 9280, avg_snowfall: 235, location: 'Summit County')
+        schoolmarm = keystone.ski_runs.create!(name: 'schoolmarm', open: true, distance: 3, green: true, blue: false,
+                                               black: false, condition: 'groomed')
+        noodle_soup = keystone.ski_runs.create!(name: 'noodle soup', open: false, distance: 6, green: false, blue: false,
+                                                black: true, condition: 'groomed')
+        expect(keystone.count_runs).to eq(2)
+      end
+    end
+  end
 end
--- spec/models/ski_run_spec.rb
+++ spec/models/ski_run_spec.rb
@@ -14,4 +14,33 @@ RSpec.describe SkiRun, type: :model do
     it { should allow_value(false).for :blue }
     it { should allow_value(false).for :black }
   end
+
+  describe 'class methods' do
+    describe '::is_open' do
+      it 'checks if the run is open' do
+        SkiResort.destroy_all
+        keystone = SkiResort.create!(name: 'Keystone', lifts: 20, backcountry_access: true, employee: 30_000,
+                                     snowboarder_permitted: true, altitude: 9280, avg_snowfall: 235, location: 'Summit County')
+        schoolmarm = keystone.ski_runs.create!(name: 'schoolmarm', open: true, distance: 3, green: true, blue: false,
+                                               black: false, condition: 'groomed')
+        noodle_soup = keystone.ski_runs.create!(name: 'noodle soup', open: false, distance: 2, green: false, blue: false,
+                                                black: true, condition: 'groomed')
+
+        expect(SkiRun.is_open).to eq([schoolmarm])
+      end
+    end
+    describe '::greater_than' do
+      it 'filters ski runs with a specific distance' do
+        SkiResort.destroy_all
+        keystone = SkiResort.create!(name: 'Keystone', lifts: 20, backcountry_access: true, employee: 30_000,
+                                     snowboarder_permitted: true, altitude: 9280, avg_snowfall: 235, location: 'Summit County')
+        schoolmarm = keystone.ski_runs.create!(name: 'schoolmarm', open: true, distance: 3, green: true, blue: false,
+                                               black: false, condition: 'groomed')
+        noodle_soup = keystone.ski_runs.create!(name: 'noodle soup', open: false, distance: 2, green: false, blue: false,
+                                                black: true, condition: 'groomed')
+
+        expect(SkiRun.greater_than(2)).to eq([schoolmarm])
+      end
+    end
+  end
 end
