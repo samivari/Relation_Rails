242000bdd184ae1399f6b3d9035b1354dd4248fe
Sam Iavri <sammi80134@gmail.com>
2022-02-09 13:49:08 -0700

    User story 22

--- spec/features/ski_resorts/delete_spec.rb
+++ spec/features/ski_resorts/delete_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'delete' do
-  it 'can delete a resort' do # 20
+  it 'can delete a resort on the show page' do # 19
     SkiResort.destroy_all
     keystone = SkiResort.create!(name: 'Keystone', lifts: 20, backcountry_access: true, employee: 30_000,
                                  snowboarder_permitted: true, altitude: 9280, avg_snowfall: 235, location: 'Summit County')
@@ -11,6 +11,22 @@ RSpec.describe 'delete' do
 
     click_link 'Delete'
 
+    expect(current_path).to eq('/ski_resorts')
+    expect(page).to_not have_content(keystone.name)
+  end
+
+  it 'can delete a resort on the index page' do # 19
+    SkiResort.destroy_all
+    keystone = SkiResort.create!(name: 'Keystone', lifts: 20, backcountry_access: true, employee: 30_000,
+                                 snowboarder_permitted: true, altitude: 9280, avg_snowfall: 235, location: 'Summit County')
+    breck = SkiResort.create!(name: 'Breckinridge', lifts: 24, backcountry_access: false, employee: 23_000,
+                              snowboarder_permitted: true, altitude: 10_280, avg_snowfall: 333, location: 'Summit County')
+
+    visit '/ski_resorts'
+
+    within("#ski-resort-#{keystone.id}") do
+      click_link 'Delete'
+    end
     expect(current_path).to eq('/ski_resorts')
     expect(page).to_not have_content(keystone.name)
   end
